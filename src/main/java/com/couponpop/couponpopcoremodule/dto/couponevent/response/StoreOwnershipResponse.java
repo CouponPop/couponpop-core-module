5749da104aeb7cf782a9b3ca78cc9a8df1285a8f
seyoung5744 <seyoung574458@gmail.com>
2025-11-05 12:37:49 +0900

    fix : OpenFeign boolean 직렬화 이슈

--- src/main/java/com/couponpop/couponpopcoremodule/dto/couponevent/response/StoreOwnershipResponse.java
+++ src/main/java/com/couponpop/couponpopcoremodule/dto/couponevent/response/StoreOwnershipResponse.java
@@ -1,6 +1,9 @@
 package com.couponpop.couponpopcoremodule.dto.couponevent.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public record StoreOwnershipResponse(
+        @JsonProperty("isOwner")
         boolean isOwner
 ) {
     public static StoreOwnershipResponse from(boolean isOwner) {
