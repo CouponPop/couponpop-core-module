144f290bd11ef956a69c0dae9ca05a19c83d8173
younghunkimm <hunifive@gmail.com>
2025-11-09 03:35:28 +0900

    fix: CouponUsageStatsFcmSendMessage의 token 리스트를 개별 토큰으로 변경

--- src/main/java/com/couponpop/couponpopcoremodule/dto/coupon/event/model/CouponUsageStatsFcmSendMessage.java
+++ src/main/java/com/couponpop/couponpopcoremodule/dto/coupon/event/model/CouponUsageStatsFcmSendMessage.java
@@ -1,16 +1,14 @@
 package com.couponpop.couponpopcoremodule.dto.coupon.event.model;
 
-import java.util.List;
-
 public record CouponUsageStatsFcmSendMessage(
         Long memberId,
-        List<String> tokens,
+        String token,
         String topDong,
         int topHour,
         int activeEventCount
 ) {
 
-    public static CouponUsageStatsFcmSendMessage of(Long memberId, List<String> tokens, String topDong, int topHour, int activeEventCount) {
-        return new CouponUsageStatsFcmSendMessage(memberId, tokens, topDong, topHour, activeEventCount);
+    public static CouponUsageStatsFcmSendMessage of(Long memberId, String token, String topDong, int topHour, int activeEventCount) {
+        return new CouponUsageStatsFcmSendMessage(memberId, token, topDong, topHour, activeEventCount);
     }
 }
