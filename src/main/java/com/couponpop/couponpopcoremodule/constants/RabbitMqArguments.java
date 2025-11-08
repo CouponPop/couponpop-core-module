735fef94f74632464a9ceba7d89e367cfabdfa38
younghunkimm <hunifive@gmail.com>
2025-11-09 00:10:12 +0900

    feat: RabbitMQ 큐 속성에 `x-message-ttl` 상수 추가

--- src/main/java/com/couponpop/couponpopcoremodule/constants/RabbitMqArguments.java
+++ src/main/java/com/couponpop/couponpopcoremodule/constants/RabbitMqArguments.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class RabbitMqArguments {
 
+    public static final String X_MESSAGE_TTL = "x-message-ttl";
     public static final String X_DEAD_LETTER_EXCHANGE = "x-dead-letter-exchange";
     public static final String X_DEAD_LETTER_ROUTING_KEY = "x-dead-letter-routing-key";
 }
