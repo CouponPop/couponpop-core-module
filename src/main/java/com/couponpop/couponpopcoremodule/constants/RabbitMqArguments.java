448669e721aa1a6a516b58ca9acbd23363874369
younghunkimm <hunifive@gmail.com>
2025-11-08 20:09:10 +0900

    feat: RabbitMQ 큐 속성 상수 추가

---
+++ src/main/java/com/couponpop/couponpopcoremodule/constants/RabbitMqArguments.java
@@ -0,0 +1,11 @@
+package com.couponpop.couponpopcoremodule.constants;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class RabbitMqArguments {
+
+    public static final String X_DEAD_LETTER_EXCHANGE = "x-dead-letter-exchange";
+    public static final String X_DEAD_LETTER_ROUTING_KEY = "x-dead-letter-routing-key";
+}
