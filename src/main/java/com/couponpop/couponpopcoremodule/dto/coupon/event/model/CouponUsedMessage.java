83de1d86066efd4db320fc5316550956146c4324
seyoung5744 <seyoung574458@gmail.com>
2025-11-10 16:26:22 +0900

    feat: CouponUsedMessage TraceId 추가

--- src/main/java/com/couponpop/couponpopcoremodule/dto/coupon/event/model/CouponUsedMessage.java
+++ src/main/java/com/couponpop/couponpopcoremodule/dto/coupon/event/model/CouponUsedMessage.java
@@ -3,6 +3,7 @@ package com.couponpop.couponpopcoremodule.dto.coupon.event.model;
 import com.couponpop.couponpopcoremodule.enums.coupon.CouponMessageType;
 
 public record CouponUsedMessage(
+        String traceId,
         Long couponId,
         Long memberId,
         Long storeId,
@@ -11,8 +12,9 @@ public record CouponUsedMessage(
         String eventName,
         CouponMessageType messageType
 ) {
-    public static CouponUsedMessage of(Long couponId, Long memberId, Long storeId, String storeName, Long eventId, String eventName, CouponMessageType messageType) {
+    public static CouponUsedMessage of(String traceId, Long couponId, Long memberId, Long storeId, String storeName, Long eventId, String eventName, CouponMessageType messageType) {
         return new CouponUsedMessage(
+                traceId,
                 couponId,
                 memberId,
                 storeId,
