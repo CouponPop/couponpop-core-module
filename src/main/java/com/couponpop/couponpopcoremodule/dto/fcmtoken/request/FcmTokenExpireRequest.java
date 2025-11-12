a59f85fa6b6df10dd47d76e7a84c0834f2b3fc36
younghunkimm <hunifive@gmail.com>
2025-11-12 16:46:03 +0900

    feat: FcmTokenExpireRequest에 memberId 필드를 추가

--- src/main/java/com/couponpop/couponpopcoremodule/dto/fcmtoken/request/FcmTokenExpireRequest.java
+++ src/main/java/com/couponpop/couponpopcoremodule/dto/fcmtoken/request/FcmTokenExpireRequest.java
@@ -1,9 +1,10 @@
 package com.couponpop.couponpopcoremodule.dto.fcmtoken.request;
 
 public record FcmTokenExpireRequest(
+        Long memberId,
         String fcmToken
 ) {
-    public static FcmTokenExpireRequest from(String fcmToken) {
-        return new FcmTokenExpireRequest(fcmToken);
+    public static FcmTokenExpireRequest of(Long memberId, String fcmToken) {
+        return new FcmTokenExpireRequest(memberId, fcmToken);
     }
 }
