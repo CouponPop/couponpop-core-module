2f392fa540743a5226419e2dec5bb247d528a2c5
seyoung5744 <seyoung574458@gmail.com>
2025-11-12 11:13:30 +0900

    feat: 쿠폰 FCM 메세지 추상화

--- src/main/java/com/couponpop/couponpopcoremodule/dto/coupon/event/model/CouponIssuedMessage.java
+++ src/main/java/com/couponpop/couponpopcoremodule/dto/coupon/event/model/CouponIssuedMessage.java
@@ -1,33 +1,31 @@
 package com.couponpop.couponpopcoremodule.dto.coupon.event.model;
 
 import com.couponpop.couponpopcoremodule.enums.coupon.CouponMessageType;
+import lombok.Getter;
+import lombok.ToString;
 
-public record CouponIssuedMessage(
-        String traceId,
-        String token,
-        Long couponId,
-        Long memberId,
-        Long storeId,
-        String storeName,
-        Long eventId,
-        int totalCount,
-        int issuedCount,
-        String eventName,
-        CouponMessageType messageType
-) {
-    public static CouponIssuedMessage of(String traceId, String token, Long couponId, Long memberId, Long storeId, String storeName, Long eventId, int totalCount, int issuedCount, String eventName, CouponMessageType messageType) {
-        return new CouponIssuedMessage(
-                traceId,
-                token,
-                couponId,
-                memberId,
-                storeId,
-                storeName,
-                eventId,
-                totalCount,
-                issuedCount,
-                eventName,
-                messageType
-        );
+import java.time.LocalDateTime;
+
+@Getter
+@ToString(callSuper = true)
+public class CouponIssuedMessage extends AbstractCouponFcmMessage {
+
+    private final String storeName;
+    private final int totalCount;
+    private final int issuedCount;
+    private final String eventName;
+    private final CouponMessageType messageType;
+
+    public CouponIssuedMessage(String traceId, String token, Long couponId, Long memberId, Long storeId, Long eventId, String storeName, String eventName, int totalCount, int issuedCount, CouponMessageType messageType, LocalDateTime occurredAt) {
+        super(traceId, token, couponId, memberId, storeId, eventId, occurredAt);
+        this.storeName = storeName;
+        this.totalCount = totalCount;
+        this.issuedCount = issuedCount;
+        this.eventName = eventName;
+        this.messageType = messageType;
+    }
+
+    public static CouponIssuedMessage of(String traceId, String token, Long couponId, Long memberId, Long storeId, Long eventId, String storeName, String eventName, int totalCount, int issuedCount, CouponMessageType messageType, LocalDateTime occurredAt) {
+        return new CouponIssuedMessage(traceId, token, couponId, memberId, storeId, eventId, storeName, eventName, totalCount, issuedCount, messageType, occurredAt);
     }
 }
